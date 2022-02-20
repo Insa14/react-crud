468fb1c6e8db342234cb16da884a805e3f82c09d
insa14 <kong3361@naver.com>
2022-02-21 01:33:38 +0900

    ➕Add Rotuer-dependency
    기본 라우터 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Nav from "./components/Nav";
+
 function App() {
   return (
-    <h1>App</h1>
+    <>
+      <Router>
+          <Nav/>
+          <Switch>
+            <Route exact path="/">{Home}</Route>
+            <Route path="/about">{About}</Route>
+          </Switch>
+      </Router>
+    </>
   );
 }
 
